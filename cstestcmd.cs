edcc984c3afd39731154cc2aee26feb534b914e5
John Comeau <jc@unternet.net>
2023-02-28 15:55:48 -0700

    this works, but I'd rather have an -S option built into gs

--- cstestcmd.cs
+++ cstestcmd.cs
@@ -1,4 +1,4 @@
-#!/usr/src/jcomeauictx/casperscript/bin/cs -q -dNODISPLAY -dBATCH --
+#!/usr/bin/env -S /usr/src/jcomeauictx/casperscript/bin/cs -q -dNODISPLAY -dBATCH --
 (testing...) =
 /ARGUMENTS dup where {(ARGUMENTS: ) print exch get ==} {pop} ifelse
 /abc (abc) def
