49f65ba9b01d3e1605bb39e5f31db5d105bbec60
John Comeau <jc@unternet.net>
2023-02-28 23:17:03 -0700

    broke it in anticipation of getting splitargs working

--- cstestcmd.cs
+++ cstestcmd.cs
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S /usr/src/jcomeauictx/casperscript/bin/cs -q -dNODISPLAY -dBATCH --
+#!/usr/src/jcomeauictx/casperscript/bin/cs -S -q -dNODISPLAY -dBATCH --
 (testing...) =
 /ARGUMENTS dup where {(ARGUMENTS: ) print exch get ==} {pop} ifelse
 /abc (abc) def
