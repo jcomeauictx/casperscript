b90d78d51eec4945cf872944a133e4781a24ef44
John Comeau <jc@unternet.net>
2023-03-01 13:30:08 -0700

    cleanup mascheck's code

--- showargs.c
+++ showargs.c
@@ -1,10 +1,9 @@
 /* https://www.in-ulm.de/~mascheck/various/shebang/ */
 #include <stdio.h>
-int main(argc, argv)
-	int argc; char** argv;
+int main(int argc, char **argv)
 {
 	int i;
 	for (i=0; i<argc; i++)
-    		fprintf(stdout, "argv[%d]: \"%s\"\n", i, argv[i]);
+		fprintf(stdout, "argv[%d]: \"%s\"\n", i, argv[i]);
 	return(0);
 }
