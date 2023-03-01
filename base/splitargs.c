34c66d0093a7dcc01d56281588e9f3988fb46635
John Comeau <jc@unternet.net>
2023-02-28 21:00:24 -0700

    still useless but compiles

--- base/splitargs.c
+++ base/splitargs.c
@@ -1,5 +1,10 @@
 /* split shebang-supplied multiple options into individual arguments */
+#include <syslog.h>
 int main(int argc, char **argv)
     {
-
+        char *splitargs[1024];  /* likely overkill but still only 1 MB */
+        int argn = 0, i;
+        for (i = 0; i < argc; i++) {splitargs[i] = argv[i]; argn++;}
+        for (i = 0; i < argn; i++) syslog(LOG_USER | LOG_DEBUG, splitargs[i]);
+    }
 // vim: tabstop=8 shiftwidth=4 expandtab softtabstop=4
