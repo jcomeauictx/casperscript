422dc3afe0791e3ddbbef2e5263265e48ff7bb64
John Comeau <jc@unternet.net>
2023-03-08 10:44:33 -0700

    testing comment '#' with no following space, works as expected

--- cstestcmd.cs
+++ cstestcmd.cs
@@ -3,4 +3,8 @@
 /ARGUMENTS dup where {(ARGUMENTS: ) print exch get ==} {pop} ifelse
 /abc (abc) def
 abc =
+#
+(this should not show, as the preceding line has no following space) =
+# 
+(def) = # this should print, since the preceding line has following space
 (final stack:) = pstack
