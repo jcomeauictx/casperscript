a94ddfe37e89ae223cc91401250857d26061b156
John Comeau <jc@unternet.net>
2023-04-13 06:40:20 -0700

    prompt bug seems to be fixed. other bugs remain.

--- base/gp_strdl.c
+++ base/gp_strdl.c
@@ -24,8 +24,10 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
-#include <termios.h>
+#include <termios.h>  /* for turning off and on echoing */
+#include <unistd.h>  /* for read() and write() */
 #include "gssyslog.h"
+#include "files.h"
 #endif
 
 extern int rl_tty_set_echoing PARAMS((int));
@@ -48,29 +50,37 @@ gp_readline(stream *s_in, stream *s_out, void *readline_data,
     return sreadline(s_in, s_out, readline_data, prompt, buf, bufmem, pcount,
                      pin_eol, is_stdin);
 #else
+/* //eli.thegreenplace.net/2016/basics-of-using-the-readline-library/ */
+/* //stackoverflow.com/a/59923166/493161 */
+#define CS_STDIN 0
+#define CS_STDOUT 1
 #define MAXPROMPT 32
 #define MAXREPLY 12
+#define MINREPLY 5  /* 5 is absolute minimum reply: <CSI>1;1R */
 #define QUERY "\033[6n"
 #define PROMPT "..............................."
-#define sane(x, max) (x > 0 && x < max)
     const byte *query = (byte *)QUERY;
-    uint querysize = strlen((char *)query);
-    /* "\033[{ROW};{COLUMN}R", minimum 5 bytes with CSI, 6 with <ESC>[ */
+    /* uint querysize = strlen((char *)query); */ /* no need using outprintf */
+    /* "\033[{ROW};{COLUMN}R", minimum 5 bytes with CSI, 6 with <ESC>[
+     * we're going to assume that 4 will be the minimum (1-based) column,
+     * and that a stack size of >999 is highly unlikely. this will mean
+     * only having to check one digit of the reply, that before the 'R'.
+     * a value less than 4 can be taken to mean: 0=10, ... 3=13.
+     * NOTE: use low-level calls here, bypassing any stream buffering */
     char reply[MAXREPLY] = "";
     uint replysize = 0;
-    /* //eli.thegreenplace.net/2016/basics-of-using-the-readline-library/ */
     char *buffer, promptstring[MAXPROMPT] = "";
     int promptsize, count = *pcount, code = EOF;
     uint nsize;
     byte *ndata;
-    struct termios settings;
+    struct termios settings[2];
 
     /* disable auto-complete with filenames */
     DISCARD(rl_bind_key('\t', rl_insert));
     syslog(LOG_USER | LOG_DEBUG, "gp_readline called with count %d, buf %.*s",
             count, count + MAXPROMPT, (char *)buf->data);
     if (prompt && prompt->size > (MAXPROMPT - 1)) {
-        code = 1;
+        code = 1;  /* FIXME: this code means buffer full, find a better one */
     } else {
         if (!count && s_out && prompt) {
             DISCARD(strncpy(promptstring, (char *)prompt->data, prompt->size));
@@ -80,27 +90,29 @@ gp_readline(stream *s_in, stream *s_out, void *readline_data,
             /* attempt to get cursor position using <ESC>[6n which replies
              * with <ESC>[{ROW};{COLUMN}R
              */
-            tcgetattr(0, &settings);
-            /* //stackoverflow.com/a/59923166/493161 */
-            settings.c_lflag &= ~ECHO;
-            tcsetattr(0, 0, &settings);
-            if (s_out) sputs(s_out, query, querysize, NULL);
-            else outprintf(bufmem, (const char *)query);
+            /*FIXME: this needs to be wrapped in timeout and ^C trap */
+            tcgetattr(CS_STDIN, &settings[0]);  /* for reference */
+            tcgetattr(CS_STDIN, &settings[1]);  /* FIXME: use memmove ? */
+            settings[1].c_lflag &= ~ECHO;  /* don't echo reply to screen */
+            settings[1].c_lflag &= ~ICANON;  /* stop buffering */
+            tcsetattr(CS_STDIN, TCSANOW, &settings[1]);
+            outprintf(bufmem, (const char *)query);
             syslog(LOG_USER | LOG_DEBUG, "sent query, waiting for reply");
+            replysize += read(CS_STDIN, reply, MINREPLY);  
             while (replysize < MAXREPLY - 1) {  /* leave a byte for \0 */
-                reply[replysize++] = sgetc(s_in);
                 if (reply[replysize - 1] == 'R') break;
-                else syslog(LOG_USER | LOG_DEBUG, "got <ESC>%.*s so far",
+                replysize += read(CS_STDIN, &reply[replysize], 1);
+                syslog(LOG_USER | LOG_DEBUG, "got <ESC>%.*s so far",
                         replysize - 1, reply + 1);
             }
             syslog(LOG_USER | LOG_DEBUG, "gp_readline query reply: <ESC>%.*s",
                     replysize - 1, reply + 1);
-            promptsize = 3;  /*FIXME: determined by column number */
+            promptsize = reply[replysize - 2] - '0';  /* convert digit to int */
+            if (promptsize < 4) promptsize += 10;
             DISCARD(strncpy(promptstring, PROMPT, promptsize));
             promptstring[promptsize - 1] = '>';
             promptstring[promptsize] = '\0';
-            settings.c_lflag |= ECHO;
-            tcsetattr(0, 0, &settings);
+            tcsetattr(CS_STDIN, TCSANOW, &settings[0]);  /* restore termios */
         }
         while ((buffer = readline(promptstring)) != NULL) {
             count = strlen(buffer);
