e73598b4212743221250733d4cd793a5637912f3
John Comeau <jc@unternet.net>
2023-04-25 18:03:45 -0700

    got rid of showpnm now that readpnm working

--- vdiff.cs
+++ vdiff.cs
@@ -1,6 +1,8 @@
 #! bin/cs -S -C --
 % visual diff two pnm files
 
+/inch {72 mul} bind def
+
 /bits <<  % translate maxvalue into bits per sample
   1 1
   255 8
@@ -61,50 +63,21 @@
   instance dup (instance: ) print === colorspace
 } bind def
 
-/showpnm {  % file - 
-  % read PNM file into dict and show on page
-  % we confine ourselves to gs-created pnm files, which only have a single
-  % whole-line comment on 2nd line. this won't work with end-of-line comments.
-  pnm dup length dict copy /instance exch def  % new instance of pnm dictionary
-  /infile exch def  % store open file handle in `infile`.
-  /buffer 1024 string def  % hopefully enough for any PNM line length
-  /pnmtype infile token pop cvlit def  % /P1, /P2, or /P3
-  instance /Decode decode pnmtype get put
-  /colorspace colorspaces pnmtype get def
-  % read and discard the expected comment, throw error on failure.
-  infile buffer readline pop 0 get comment ne {
-    (Not gs-created pnm file) = /valueerror signalerror
-  } if
-  instance /Width infile token pop cvi dup /width exch def put
-  instance /Height infile token pop cvi dup /height exch def put
-  instance /ImageMatrix get dup dup  % we're going to overwrite the /w etc.
-    0 width put
-    3 height neg put
-    5 height put
-  pnmtype (P1) ne {  % P2 and P3 have an extra line, max value
-    instance /BitsPerComponent bits infile token pop cvi get put
-  } if
-  instance /DataSource {
-    infile token {1 string dup 0 4 -1 roll put} {()} ifelse
-  } /ReusableStreamDecode filter put
-  instance dup (instance: ) print === colorspace
-  setcolorspace 
-  width height scale
-  image
-} bind def
-
 /vdiff {  % filename1 filename2 sidebyside -
   % generate pnm of differences between two pnm files
   % (vdiff called, stack:) = pstack
   /sidebyside exch def
   /filename2 exch def
   /filename1 exch def
-  filename1 36 36 translate readpnm
+  filename1 .25 inch .25 inch translate readpnm
   setcolorspace
   dup /Width get exch dup /Height get exch 3 1 roll scale dup image
   /DataSource get dup resetfile bytesavailable (data length: ) print =
   showpage
-  filename2 (r) file 36 36 translate showpnm
+  filename2 .25 inch .25 inch translate readpnm
+  setcolorspace
+  dup /Width get exch dup /Height get exch 3 1 roll scale dup image
+  /DataSource get dup resetfile bytesavailable (data length: ) print =
   showpage
   (pstack at end: ) = pstack
 } bind def
